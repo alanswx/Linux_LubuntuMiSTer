c74fc8051fcfb1a5db479cddc742c5a9dcfa3c0c
Alan Steremberg <alan.steremberg@gmail.com>
2018-04-02 10:33:35 -0700

    fixes

--- createimage.sh
+++ createimage.sh
@@ -11,5 +11,14 @@ multistrap -a armhf -f multistrap.conf -d $TARGETDIR
 cp /usr/bin/qemu-arm-static ${TARGETDIR}/usr/bin
 export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 LC_ALL=C LANGUAGE=C LANG=C chroot $TARGETDIR dpkg --configure -a
+sudo chroot $TARGETDIR /var/lib/dpkg/info/dash.preinst
+LC_ALL=C LANGUAGE=C LANG=C chroot $TARGETDIR dpkg --configure -a
+chroot $TARGETDIR update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+rsync -r extras/ $TARGETDIR
+#chroot $TARGETDIR passwd
+chroot $TARGETDIR useradd mister -m -g sudo -G sudo 
+chroot $TARGETDIR chpasswd << 'END'
+mister:mister
+END
 umount $TARGET_DIR
 losetup -d $LOOPNAME
